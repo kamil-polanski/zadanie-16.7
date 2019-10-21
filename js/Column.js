5a589f6ea18903b029ece5810ed1f6a88e6389fe
Kamil Polanski <polanski88@gmail.com>
2019-10-22 00:06:40 +0200

    Modify column

--- js/Column.js
+++ js/Column.js
@@ -1,26 +1,50 @@
-function Column(name) {
-	var self = this;
+function Column(id, name) {
+	const self = this;
+	this.id = id;
+	this.name = name || 'No name given';
 
-	this.id = randomString();
-	this.name = name;
 	this.element = generateTemplate('column-template', { name: this.name, id: this.id });
-
 	this.element.querySelector('.column').addEventListener('click', function (event) {
 		if (event.target.classList.contains('btn-delete')) {
-				self.removeColumn();
+			self.removeColumn();
 		}
 
 		if (event.target.classList.contains('add-card')) {
-				self.addCard(new Card(prompt("Enter the name of the card")));
+			const cardName = prompt('Enter the name of the card');
+			event.preventDefault();
+
+			const data = new FormData();
+			data.append('name', cardName);
+			data.append('bootcamp_kanban_column_id', self.id);
+
+			fetch(baseUrl + '/card', {
+				method: 'POST',
+				headers: myHeaders,
+				body: data,
+			})
+				.then(function (res) {
+					return res.json();
+				})
+				.then(function (resp) {
+					const card = new Card(resp.id, cardName);
+					self.addCard(card);
+				});
 		}
-});
+	})
 }
 
 Column.prototype = {
-addCard: function(card) {
-	this.element.querySelector('ul').appendChild(card.element);
-},
-removeColumn: function() {
-	this.element.parentNode.removeChild(this.element);
+	addCard: function (card) {
+		this.element.querySelector('ul').appendChild(card.element);
+	},
+	removeColumn: function () {
+		const self = this;
+		fetch(baseUrl + '/column/' + self.id, { method: 'DELETE', headers: myHeaders })
+			.then(function (resp) {
+				return resp.json();
+			})
+			.then(function (resp) {
+				self.element.parentNode.removeChild(self.element);
+			});
+	}
 }
-};
