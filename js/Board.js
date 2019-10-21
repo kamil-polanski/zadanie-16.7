4e5bf38a3ff0dda263a902cbcd71da8b9db38744
Kamil Polanski <polanski88@gmail.com>
2019-10-22 00:21:23 +0200

    Modify board

--- js/Board.js
+++ js/Board.js
@@ -1,21 +1,35 @@
-var board = {
+const board = {
   name: 'Tablica Kanban',
-  addColumn: function(column) {
+  addColumn: function (column) {
     this.element.appendChild(column.element);
-    initSortable(column.id); //About this feature we will tell later
+    initSortable(column.id);
   },
   element: document.querySelector('#board .column-container')
 };
 
-document.querySelector('#board .create-column').addEventListener('click', function() {
-  var name = prompt('Enter a column name');
-  var column = new Column(name);
-  board.addColumn(column);
+document.querySelector('#board .create-column').addEventListener('click', function () {
+  const name = prompt('Enter a column name');
+  const data = new FormData();
+
+  data.append('name', name);
+
+  fetch(baseUrl + '/column', {
+    method: 'POST',
+    headers: myHeaders,
+    body: data,
+  })
+    .then(function (resp) {
+      return resp.json();
+    })
+    .then(function (resp) {
+      const column = new Column(resp.id, name);
+      board.addColumn(column);
+    });
 });
 
 function initSortable(id) {
-  var el = document.getElementById(id);
-  var sortable = Sortable.create(el, {
+  const el = document.getElementById(id);
+  const sortable = Sortable.create(el, {
     group: 'kanban',
     sort: true
   });
